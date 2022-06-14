e3db7c749a81146cd88e886239a045e501355a0d
inheert <theoclaereboudtgmail.Com>
2022-06-14 19:38:34 +0200

    début du script de récupération des articles pubmed

--- PubMed.py
+++ PubMed.py
@@ -1,19 +1,72 @@
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import os
+import glob
 import time
 from uuid import uuid4
 
+
 class Pubmed:
 
-    def __init__(self, url):
-        self.url = url
+    def __init__(self, url: str):
+        os.environ["GH_TOKEN"] = "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"
+
         self.uid = uuid4().hex
+        self.directory = f"{os.path.abspath(os.curdir)}/data/{self.uid}"
 
+        self.url = url
+        self.total_pages = None
+        self.actual_page = 1
 
-    def _Initialize(self):
-        pass
+        self.service = Service(executable_path=ChromeDriverManager().install())
+        self.options = Options()
+        prefs = {"download.default_directory": self.directory}
+        self.options.add_experimental_option("prefs", prefs)
+
+    def RetrieveArticles(self):
+
+        driver = webdriver.Chrome(service=self.service, chrome_options=self.options)
+        driver.implicitly_wait(1)
+
+        driver.get(self.url)
+
+        if not self.total_pages:
+            total_pages = driver.find_element(by=By.CLASS_NAME, value="of-total-pages")
+            self.total_pages = int(total_pages.text.split(" ")[1].strip())
+            print(self.total_pages, type(self.total_pages))
+
+        while self.actual_page <= self.total_pages:
+            self._RetrieveArticle(driver)
+
+        time.sleep(10)
+        driver.quit()
+
+    def _RetrieveArticle(self, _driver):
+
+        button = _driver.find_element(by=By.ID, value='save-results-panel-trigger')
+        button.click()
+
+        select = Select(_driver.find_element(by=By.ID, value='save-action-selection'))
+        select.select_by_visible_text('All results on this page')
+
+        select = Select(_driver.find_element(by=By.ID, value='save-action-format'))
+        select.select_by_visible_text('PubMed')
+
+        button2 = _driver.find_element(by=By.CLASS_NAME, value='action-panel-submit')
+        _driver.execute_script("arguments[0].click();", button2)
+
+        self.url = self.url.replace(f"page={self.actual_page}", f"page={self.actual_page + 1}")
+        self.actual_page += 1
+
+    def UnifyFiles(self):
+
+        dictionary = {"AB": "", "AD": "", "AID": "", "DP": "", "FAU": "", "IR": "", "JT": "", "MH": "", "OT": "",
+                      "PL": "", "PMID": "", "PT": "", "RN": "", "TI": ""}
+
+        for file in glob.glob(f"{self.directory}/*.txt"):
+            file = open(file, "r").read()
+            print(file)
--- main.py
+++ main.py
@@ -1,42 +1,41 @@
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.by import By
-from webdriver_manager.firefox import GeckoDriverManager
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-import os
-import time
+import pandas as pd
+from PubMed import *
 
-os.environ["GH_TOKEN"] = "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"
+# first = Pubmed("https://pubmed.ncbi.nlm.nih.gov/?term=%28autoimmune+thyroid+disease%29+AND+%28neplasms%29&filter=hum_ani.humans&filter=years.2000-2022&size=200&page=1")
+# second = Pubmed("https://pubmed.ncbi.nlm.nih.gov/?term=%28euthyroid+sick+syndrome%29&size=200")
+#
+# first.RetrieveArticles()
+# first.UnifyFiles()
+
+dictionary = {"AB": [], "AD": [], "AID": [], "DP": [], "FAU": [], "IR": [], "JT": [], "MH": [], "OT": [],
+              "PL": [], "PMID": [], "PT": [], "RN": [], "TI": []}
+
+valid = ["AB", "AD", "AID", "DP", "FAU", "IR", "JT", "MH", "OT", "PL", "PMID", "PT", "RN", "TI"]
+
+file = open("data/75e9bfb0f24a4667ae37562c2579a6e2/pubmed-autoimmune-set.txt", "r")
+articles = file.read().split("\n\n")
+
+for article in articles:
+
+    article_dictionary = {"AB": "", "AD": "", "AID": "", "DP": "", "FAU": "", "IR": "", "JT": "", "MH": "", "OT": "",
+                  "PL": "", "PMID": "", "PT": "", "RN": "", "TI": ""}
+
+    article = article.split("\n")
+
+    for tag in article:
+        tag = tag.split("- ")
+
+        if tag[0].strip() not in valid:
+            pass
+        else:
+            # Séparateur non fonctionnel revoir cette partie
+            article_dictionary[tag[0].strip()] += tag[1] if len(article_dictionary[tag[0].strip()]) > 1 else "$#$"+tag[1]
+
+    for k, v in article_dictionary.items():
+        dictionary[k].append(v)
 
 
-service = Service(executable_path=GeckoDriverManager().install())
+df = pd.DataFrame(dictionary)
+df.to_csv("csv_test.csv")
 
-directory = f"{os.path.abspath(os.curdir)}/testSave"
 
-options = Options()
-options.set_preference("browser.download.folderList", 2)
-options.set_preference("browser.download.manager.showWhenStarting", False)
-options.set_preference("browser.download.dir", directory)
-options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/x-gzip")
-
-driver = webdriver.Firefox(service=service, options=options)
-driver.implicitly_wait(0.5)
-
-driver.get("https://pubmed.ncbi.nlm.nih.gov/?term=thyroid")
-
-button = driver.find_element(by=By.ID, value='save-results-panel-trigger')
-button.click()
-
-select = Select(driver.find_element(by=By.ID, value='save-action-selection'))
-select.select_by_visible_text('All results')
-
-select = Select(driver.find_element(by=By.ID, value='save-action-format'))
-select.select_by_visible_text('PubMed')
-
-button2 = driver.find_element(by=By.CLASS_NAME, value='action-panel-submit')
-button2.click()
-
-time.sleep(60)
-
-driver.quit()
