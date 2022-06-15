943f8290dea0a18d5771424ea4330ffbf29279e6
Inheert <theoclaereboudt@gmail.com>
2022-06-15 21:41:14 +0200

    pubmed articles cleaning and csv creation

--- PubMed.py
+++ PubMed.py
@@ -2,71 +2,131 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import ElementClickInterceptedException
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import os
+import platform
 import glob
 import time
 from uuid import uuid4
-
+import pandas as pd
 
 class Pubmed:
 
-    def __init__(self, url: str):
+    all_tag = ["PMID", "OWN", "STAT", "DCOM", "LR", "IS", "VI", "DP", "TI", "PG", "LID", "AB", "CI", "FAU",
+               "AU", "AD", "LA", "PT", "DEP", "PL", "TA", "JT", "JID", "SB", "MH", "OTO", " OT", "EDAT", "MHDA",
+               "CRDT", "PHST", "AID", "PST", "SO"]
+
+    valid_tag = ["AB", "AD", "AID", "DP", "FAU", "IR", "JT", "MH", "OT", "PL", "PMID", "PT", "RN", "TI"]
+
+    def __init__(self, keyword: str):
         os.environ["GH_TOKEN"] = "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"
 
         self.uid = uuid4().hex
-        self.directory = f"{os.path.abspath(os.curdir)}/data/{self.uid}"
+        self.directory = f"{os.path.abspath(os.curdir)}/data/temp/{self.uid}"
+        self.directory = self.directory if platform.system() == "Linux" else self.directory.replace("/", "\\")
 
-        self.url = url
-        self.total_pages = None
-        self.actual_page = 1
+        self.url = f"https://pubmed.ncbi.nlm.nih.gov/?term={keyword}&filter=hum_ani.humans&filter=years.2000-2022&size=200&page=1"
+        self.final_dataframe = None
 
         self.service = Service(executable_path=ChromeDriverManager().install())
         self.options = Options()
+        self.options.add_argument("--disable-notifications")
         prefs = {"download.default_directory": self.directory}
         self.options.add_experimental_option("prefs", prefs)
 
     def RetrieveArticles(self):
-
-        driver = webdriver.Chrome(service=self.service, chrome_options=self.options)
-        driver.implicitly_wait(1)
+        driver = webdriver.Chrome(service=self.service, options=self.options)
+        driver.implicitly_wait(0.2)
 
         driver.get(self.url)
 
-        if not self.total_pages:
-            total_pages = driver.find_element(by=By.CLASS_NAME, value="of-total-pages")
-            self.total_pages = int(total_pages.text.split(" ")[1].strip())
-            print(self.total_pages, type(self.total_pages))
+        while True:
+            try:
+                self._SeleniumActions(driver)
+            except ElementClickInterceptedException:
+                break
 
-        while self.actual_page <= self.total_pages:
-            self._RetrieveArticle(driver)
-
-        time.sleep(10)
         driver.quit()
 
-    def _RetrieveArticle(self, _driver):
+        self.final_dataframe = self._UnifyFiles()
+        self.final_dataframe.to_csv("final_csv.csv")
 
-        button = _driver.find_element(by=By.ID, value='save-results-panel-trigger')
-        button.click()
+    @staticmethod
+    def _SeleniumActions(_driver):
 
-        select = Select(_driver.find_element(by=By.ID, value='save-action-selection'))
-        select.select_by_visible_text('All results on this page')
+        open_save_window = _driver.find_element(by=By.ID, value='save-results-panel-trigger')
+        open_save_window.click()
 
-        select = Select(_driver.find_element(by=By.ID, value='save-action-format'))
-        select.select_by_visible_text('PubMed')
+        select_result = Select(_driver.find_element(by=By.ID, value='save-action-selection'))
+        select_result.select_by_visible_text('All results on this page')
 
-        button2 = _driver.find_element(by=By.CLASS_NAME, value='action-panel-submit')
-        _driver.execute_script("arguments[0].click();", button2)
+        select_format = Select(_driver.find_element(by=By.ID, value='save-action-format'))
+        select_format.select_by_visible_text('PubMed')
 
-        self.url = self.url.replace(f"page={self.actual_page}", f"page={self.actual_page + 1}")
-        self.actual_page += 1
+        save_button = _driver.find_element(by=By.CLASS_NAME, value='action-panel-submit')
+        _driver.execute_script("arguments[0].click();", save_button)
 
-    def UnifyFiles(self):
+        next_page = _driver.find_element(by=By.XPATH, value="//div[@class='top-pagination']//button[@class='button-wrapper next-page-btn']")
+        next_page.click()
 
-        dictionary = {"AB": "", "AD": "", "AID": "", "DP": "", "FAU": "", "IR": "", "JT": "", "MH": "", "OT": "",
-                      "PL": "", "PMID": "", "PT": "", "RN": "", "TI": ""}
+    def _UnifyFiles(self):
 
-        for file in glob.glob(f"{self.directory}/*.txt"):
-            file = open(file, "r").read()
-            print(file)
+        dataframe_list = self._TransformTxtToDataframe()
+
+        final_df = None
+        for df in dataframe_list:
+            print("actual df:", df.shape)
+            if final_df is None:
+                final_df = df
+            else:
+                final_df = pd.concat([final_df, df])
+
+        print(final_df.shape)
+        return final_df.reset_index(drop=True)
+
+    def _TransformTxtToDataframe(self):
+
+        object_files = glob.glob(f"{self.directory}/*.txt") if platform.system() == "Linux" else glob.glob(f"{self.directory}\\*.txt")
+        dataframe_list = []
+
+        for file in object_files:
+            file = open(file, "r", encoding='utf-8').read()
+
+            self.dictionary = {"AB": [], "AD": [], "AID": [], "DP": [], "FAU": [], "IR": [], "JT": [], "MH": [], "OT": [],
+                          "PL": [], "PMID": [], "PT": [], "RN": [], "TI": []}
+
+            articles = file.split("\n\n")
+
+            for article in articles:
+
+                article_dictionary = {"AB": "", "AD": "", "AID": "", "DP": "", "FAU": "", "IR": "", "JT": "", "MH": "",
+                                      "OT": "",
+                                      "PL": "", "PMID": "", "PT": "", "RN": "", "TI": ""}
+
+                article = article.split("\n")
+
+                last_tag = ""
+                for tag in article:
+                    tag = tag.split("- ")
+                    name_tag = tag[0].strip()
+                    last_tag = tag[0].strip() if tag[0].strip() in Pubmed.all_tag else last_tag
+
+                    if name_tag not in Pubmed.valid_tag:
+                        if name_tag not in Pubmed.all_tag and last_tag in Pubmed.valid_tag:
+                            article_dictionary[last_tag] += f" {name_tag}"
+                        else:
+                            pass
+                    else:
+                        article_dictionary[name_tag] += tag[1] if len(article_dictionary[name_tag]) < 1 else "$#$" + \
+                                                                                                             tag[1]
+                        last_tag = name_tag
+
+                for k, v in article_dictionary.items():
+                    self.dictionary[k].append(v)
+
+            df = pd.DataFrame(self.dictionary)
+            dataframe_list.append(df)
+
+        return dataframe_list
--- main.py
+++ main.py
@@ -1,41 +1,17 @@
 import pandas as pd
 from PubMed import *
 
-# first = Pubmed("https://pubmed.ncbi.nlm.nih.gov/?term=%28autoimmune+thyroid+disease%29+AND+%28neplasms%29&filter=hum_ani.humans&filter=years.2000-2022&size=200&page=1")
-# second = Pubmed("https://pubmed.ncbi.nlm.nih.gov/?term=%28euthyroid+sick+syndrome%29&size=200")
-#
-# first.RetrieveArticles()
-# first.UnifyFiles()
-
-dictionary = {"AB": [], "AD": [], "AID": [], "DP": [], "FAU": [], "IR": [], "JT": [], "MH": [], "OT": [],
-              "PL": [], "PMID": [], "PT": [], "RN": [], "TI": []}
-
-valid = ["AB", "AD", "AID", "DP", "FAU", "IR", "JT", "MH", "OT", "PL", "PMID", "PT", "RN", "TI"]
-
-file = open("data/75e9bfb0f24a4667ae37562c2579a6e2/pubmed-autoimmune-set.txt", "r")
-articles = file.read().split("\n\n")
-
-for article in articles:
-
-    article_dictionary = {"AB": "", "AD": "", "AID": "", "DP": "", "FAU": "", "IR": "", "JT": "", "MH": "", "OT": "",
-                  "PL": "", "PMID": "", "PT": "", "RN": "", "TI": ""}
-
-    article = article.split("\n")
-
-    for tag in article:
-        tag = tag.split("- ")
-
-        if tag[0].strip() not in valid:
-            pass
-        else:
-            # Séparateur non fonctionnel revoir cette partie
-            article_dictionary[tag[0].strip()] += tag[1] if len(article_dictionary[tag[0].strip()]) > 1 else "$#$"+tag[1]
-
-    for k, v in article_dictionary.items():
-        dictionary[k].append(v)
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import ElementClickInterceptedException
 
 
-df = pd.DataFrame(dictionary)
-df.to_csv("csv_test.csv")
+first = Pubmed("%28autoimmune+thyroid+disease%29+AND+%28neplasms%29")
+first.RetrieveArticles()
+
 
 
